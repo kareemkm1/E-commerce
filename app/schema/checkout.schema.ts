80d3f45e4c8938378d778635f1d3fa375bd42bba
kareemkm1 <temoozztemoozz@gmail.com>
2026-01-08 19:54:33 +0200

    update regex

--- app/schema/checkout.schema.ts
+++ app/schema/checkout.schema.ts
@@ -4,6 +4,6 @@ export const checkoutSchema = z.object({
 
     details: z.string().nonempty("Details are required"),
     city: z.string().nonempty("City is required"),
-    phone: z.string().nonempty("Phone number is required").regex(/^01[0251][0-9]{8}$/),
+    phone: z.string().nonempty("Phone number is required").regex(/^(\+20|0)?1[0125]\d{8}$|^\+[1-9]\d{1,14}$/),
 
 })
--- app/schema/register.schema.ts
+++ app/schema/register.schema.ts
@@ -1,11 +1,11 @@
 import * as z from 'zod';
 
 export const registerSchema = z.object({
-    name: z.string().nonempty("Name is required").min(3, "Name must be at least 3 characters long").max(10, "Name must be at most 10 characters long"),
+    name: z.string().nonempty("Name is required").min(3, "Name must be at least 3 characters long").max(20, "Name must be at most 20 characters long"),
     email: z.string().nonempty("Email is required").regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
     password: z.string().nonempty("Password is required").min(6, "Password must be at least 6 characters long"),
     rePassword: z.string().nonempty("Please re-enter your password"),
-    phone: z.string().nonempty("Phone number is required").regex(/^01[0251][0-9]{8}$/),
+    phone: z.string().nonempty("Phone number is required").regex(/^(\+20|0)?1[0125]\d{8}$|^\+[1-9]\d{1,14}$/),
 }).refine((object) => object.password === object.rePassword, {
     path: ["rePassword"],
     message: "Passwords do not match"
